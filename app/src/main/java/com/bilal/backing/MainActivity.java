9738a6c97689e3b4e6417483cf32e9b968aed6f6
Bilal Ibrahim Hairab <bilal.hairab@gmail.com>
2018-04-24 22:52:59 +0200

    add libraries

--- app/src/main/java/com/bilal/backing/MainActivity.java
+++ app/src/main/java/com/bilal/backing/MainActivity.java
@@ -2,12 +2,22 @@ package com.bilal.backing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.GridView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.gv_recipes)
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Log.e("bilal_columns", gridView.getNumColumns() + "");
     }
 }
